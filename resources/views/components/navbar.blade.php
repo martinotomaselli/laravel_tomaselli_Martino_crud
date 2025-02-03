0373b9ecbe16f542a57651ee26aaf70780f00cbb
Martino <tomaselli.martino@gmail.com>
2025-02-03 18:16:07 +0100

    solo parte iniziale

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,9 @@
+<div class="container">
+<nav class="navbar bg-infocc ">
+  <a class="nav-link " aria-current="page" href="/">homepage</a>
+  <a class="nav-link" href="#">Link</a>
+  <a class="nav-link" href="#">Link</a>
+  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+</nav>
+
+</div>
